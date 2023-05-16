9983c034cc14dee70d83adc5ef86ed2b91c7c261
Amirhosein <amirhmch@hotmail.com>
2023-05-16 17:44:22 +0330

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ChaoticGamesWeb';
+  title = 'Chaotic Games';
+
+  @ViewChild('email', { static: false }) email!: ElementRef;
+  @ViewChild('empty', { static: false }) empty!: ElementRef;
+  @ViewChild('sub', { static: false }) sub!: ElementRef;
+
+  async oops() {
+    if (this.email.nativeElement.value.length > 0) {
+
+      if (this.email.nativeElement.value.length === 1) {
+
+        this.sub.nativeElement.setAttribute('disabled', true);
+        this.empty.nativeElement.innerHTML = 'زرنگ! ایمیلت رو درست بنویس';
+        document.querySelector('.empty')?.classList.remove('d-op');
+
+        await setTimeout(() => {
+
+          this.sub.nativeElement.removeAttribute('disabled', true);
+          document.querySelector('.empty')?.classList.add('d-op');
+
+        }, 1000);
+      } else if (!this.email.nativeElement.value.includes("@") || !this.email.nativeElement.value.includes(".")) {
+
+        this.sub.nativeElement.setAttribute('disabled', true);
+        this.empty.nativeElement.innerHTML = 'ایمیل بلد نیستی؟';
+        document.querySelector('.empty')?.classList.remove('d-op');
+
+        await setTimeout(() => {
+
+          this.sub.nativeElement.removeAttribute('disabled', true);
+          document.querySelector('.empty')?.classList.add('d-op');
+
+        }, 1000);
+      } else {
+        document.querySelector('.oops')?.classList.remove('d-none');
+      }
+
+    } else {
+
+      this.empty.nativeElement.innerHTML = 'ایمیلت رو وارد کن';
+      this.sub.nativeElement.setAttribute('disabled', true);
+      document.querySelector('.empty')?.classList.remove('d-op');
+
+      await setTimeout(() => {
+
+        this.sub.nativeElement.removeAttribute('disabled', true);
+        document.querySelector('.empty')?.classList.add('d-op');
+
+      }, 1000);
+    }
+  }
+  return() {
+    document.querySelector('.oops')?.classList.add('d-none');
+  }
+
+
 }
